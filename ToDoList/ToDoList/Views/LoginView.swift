b86fab13febb3ec4278162f96be68dbdb68b8804
zach bennett <zachzbennett@gmail.com>
2024-11-21 20:41:31 -0700

    more stuff

--- ToDoList/ToDoList/Views/LoginView.swift
+++ ToDoList/ToDoList/Views/LoginView.swift
@@ -8,8 +8,70 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var email=""
+    @State var pass=""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            // // // ///
+            // header //
+            // // // ///
+            ZStack {
+                RoundedRectangle(cornerRadius: 0)
+                    .foregroundStyle(Color.pink)
+                    .rotationEffect(Angle(degrees: 15))
+                    .offset(y:-100)
+                VStack {
+                    Text("To Do List")
+                        .foregroundStyle(Color.white)
+                        .font(.system(size: 50))
+                        .bold()
+                    Text("Get Things Done")
+                        .font(.system(size: 30))
+                        .foregroundStyle(Color.white)
+                }
+                .offset(y:-65)
+            }
+            .frame(width: UIScreen.main.bounds.width * 3, height:300)
+            
+            // // // // ////
+            // login form //
+            // // // // ////
+            ZStack {
+                RoundedRectangle(cornerRadius: 0)
+                    .backgroundStyle(Color.white)
+                Form {
+                    TextField("Email Address", text: $email)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    SecureField("Password", text: $pass)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Button {
+                        // attempt login
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundStyle(Color.blue)
+                            Text("Log in")
+                                .foregroundStyle(Color.white)
+                                .bold()
+                        }
+                    }
+                }
+                .scrollContentBackground(.hidden)
+                .background(Color("ColorBackground"))
+            }
+            .background(Color("ColorBackground"))
+            
+            // // // // // // //
+            // create account //
+            // // // // // // //
+            VStack {
+                Text("New around here?")
+                Button ("Create An Account") {
+                    // Show registration
+                }
+            }
+        }
+        .background(Color("ColorBackground"))
     }
 }
 
